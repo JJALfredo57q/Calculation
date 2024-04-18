051a28a1fd74266c40b9d8ae322e8daf49261899
JJALfredo57q <115348840+JJALfredo57q@users.noreply.github.com>
2024-04-19 00:13:49 +0800

    Add files via upload

---
+++ Func.js
@@ -0,0 +1,10 @@
+document.getElementById('Form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var num1 = document.getElementById ('number').value;
+    var num2 = document.getElementById ('number2').value;
+    var operator = document.getElementById ('operator').value;
+
+    if 
+
+})
